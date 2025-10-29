5ce5c2d5b9b4b40a33e680333ff9096bc53af579
khubaib shah <bewithbaacha@gmail.com>
2025-10-29 20:01:51 +0500

    Add UserCard component and implement AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,21 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row ">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Students" />
+          <UserCard type="Teachers" />
+          <UserCard type="Parents" />
+          <UserCard type="Staffs" />
+        </div>{" "}
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;

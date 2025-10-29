3ba009680d3e83d2e0d2a0c6256bc79be166906d
khubaib shah <bewithbaacha@gmail.com>
2025-10-29 17:50:38 +0500

    Add initial structure for school management dashboard
    - Created new pages for Admin, Parent, Student, and Teacher.
    - Implemented layout for the dashboard with a sidebar menu and navbar.
    - Added global styles using Tailwind CSS.
    - Introduced temporary data for teachers, students, parents, subjects, classes, lessons, exams, assignments, results, events, and announcements.
    - Added icons for various menu items.
    - Configured Tailwind CSS and TypeScript settings.
    - Included favicon and several images for the dashboard.

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "School Management Dashboard | Smart School System",
+  description:
+    "A comprehensive School Management Dashboard designed to simplify administration, streamline attendance tracking, manage classes and staff, and enhance overall academic performance — all in one powerful platform.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

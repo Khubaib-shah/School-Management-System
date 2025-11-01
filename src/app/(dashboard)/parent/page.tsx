31c6de60c8d451b9d52a20eee2479f8db4c418c1
khubaib shah <bewithbaacha@gmail.com>
2025-11-01 12:24:09 +0500

    ParentPage and TeacherPage to include BigCalendar and Announcement components with layout adjustments

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+
 const ParentPage = () => {
-  return <div className="">ParentPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row ">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3 flex flex-col gap-8">
+        <h2 className="text-xl font-bold">Schedule (John Doe)</h2>
+        <BigCalendar />
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcement />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;
